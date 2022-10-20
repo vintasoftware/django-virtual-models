6f600d1902ac20e6c377b42965e2389c5ecb7029
Flávio Juvenal <flavio@vinta.com.br>
2022-10-20 16:19:33 -0700

    Bump version: 0.1.1 → 0.1.2

--- django_virtual_models/__init__.py
+++ django_virtual_models/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for django_virtual_models."""
 import logging
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 # Good practice: https://docs.python-guide.org/writing/logging/#logging-in-a-library
 logging.getLogger(__name__).addHandler(logging.NullHandler())
